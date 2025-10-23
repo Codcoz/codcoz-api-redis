823e0ce87ef30858d1d0173fd12201def9700ed1
bruggalvao <brunliestudos@gmail.com>
2025-10-23 15:13:17 -0300

    Correção das rotas da API, adição de empresa_id paras rotas necessárias e adição de configurações gerais sobre o histórico de baixas

--- app.py
+++ app.py
@@ -18,41 +18,52 @@ TEMPO_TTL = 86400
 CHAVE_HISTORICO_BAIXAS = "historico_baixas"
 CHAVE_CONFIG_HISTORICO_BAIXAS = "config_historico_baixas"
 
-def rd_setar_tempo_ttl(dias: int) -> bool:
-    result_set = r.set(f"{CHAVE_CONFIG_HISTORICO_BAIXAS}:dias_ttl", str(dias))
+def rd_buscar_configs(empresa_id) -> list:
+    result_set = r.hgetall(f"{CHAVE_CONFIG_HISTORICO_BAIXAS}:{str(empresa_id)}")
     return result_set
 
-def rd_excluir_registro(id) -> bool:
-    num_chaves_excluidas = r.delete(f"{CHAVE_HISTORICO_BAIXAS}:{str(id)}")
+def rd_setar_configs(dias: int, empresa_id) -> bool:
+    result_set = r.hset(f"{CHAVE_CONFIG_HISTORICO_BAIXAS}:{str(empresa_id)}", mapping={"dias": str(dias)})
+    return result_set
+
+def rd_excluir_registro(historico_id, empresa_id) -> bool:
+    num_chaves_excluidas = r.delete(f"{CHAVE_HISTORICO_BAIXAS}:{str(empresa_id)}:{str(historico_id)}")
 
     return True if num_chaves_excluidas > 0 else False
 
-def rd_setar_registro(item_historico: dict) -> int:
+def rd_excluir_todos_registros() -> bool:
+    try:
+        return r.flushall()  # True se tiver deletado tudo
+    except Exception as e:
+        print(f"Erro ao limpar todas as chaves do Redis: {e}")
+        return False
+
+def rd_setar_registro(item_historico: dict, empresa_id) -> int:
     # Incrementando o "id" serialmente
-    id = r.incr(f"{CHAVE_HISTORICO_BAIXAS}:counter")
-    item_historico["id"] = str(id)
-    num_campos_adicionados = r.hset(f"{CHAVE_HISTORICO_BAIXAS}:{id}", mapping=item_historico)
+    historico_id = r.incr(f"{CHAVE_HISTORICO_BAIXAS}:counter")
+    item_historico["id"] = str(historico_id)
+    num_campos_adicionados = r.hset(f"{CHAVE_HISTORICO_BAIXAS}:{str(empresa_id)}:{historico_id}", mapping=item_historico)
 
     # Setando o tempo de expiração de uma chave
-    dias_ttl = r.get(f"{CHAVE_CONFIG_HISTORICO_BAIXAS}:dias_ttl")
+    dias_ttl = r.hget(f"{CHAVE_CONFIG_HISTORICO_BAIXAS}:{str(empresa_id)}", "dias")
     if dias_ttl == None:
         dias_ttl = 30
     else:
         dias_ttl = int(dias_ttl)
     
-    r.expire(f"{CHAVE_HISTORICO_BAIXAS}:{id}", dias_ttl * TEMPO_TTL)
+    r.expire(f"{CHAVE_HISTORICO_BAIXAS}:{historico_id}", dias_ttl * TEMPO_TTL)
     
-    return id if num_campos_adicionados > 0 else 0
+    return historico_id if num_campos_adicionados > 0 else 0, dias_ttl
 
-def rd_buscar_registro_por_id(id) -> dict:  
-    return r.hgetall(f"{CHAVE_HISTORICO_BAIXAS}:{str(id)}")
+def rd_buscar_registro_por_id(historico_id, empresa_id) -> dict:  
+    return r.hgetall(f"{CHAVE_HISTORICO_BAIXAS}:{str(empresa_id)}:{str(historico_id)}")
 
-def rd_buscar_todos_registros() -> list:
-    keys = r.keys(f"{CHAVE_HISTORICO_BAIXAS}:*")
+def rd_buscar_todos_registros(empresa_id) -> list:
+    keys = r.keys(f"{CHAVE_HISTORICO_BAIXAS}:{str(empresa_id)}*")
     resultados = []
 
     for k in keys:
-        if k != f"{CHAVE_HISTORICO_BAIXAS}:counter":
+        if k != f"{CHAVE_HISTORICO_BAIXAS}:{str(empresa_id)}:counter":
             value = r.hgetall(k)
             resultados.append(value)
 
@@ -88,19 +99,28 @@ def rd_filtrar_registros(dados: list, tipo_registro: str, periodo: str, tipo_ord
 
     return filtrados
 
-
-@app.route("/config/set", methods=["POST"])
-def set_config_value() -> bool:
+@app.route("/api/v1/empresa/<empresa_id>/historico_baixas/config", methods=["POST"])
+def set_config_value(empresa_id) -> bool:
     data = request.get_json()
 
     dias_expiracao = data.get("dias_expiracao")
 
-    result_set = rd_setar_tempo_ttl(dias_expiracao)
+    rd_setar_configs(dias=dias_expiracao, empresa_id=empresa_id)
 
-    return result_set
+    return jsonify({"message": f"Configuração alterada para a empresa com o id: {empresa_id}"})
 
-@app.route("/set", methods=["POST"])
-def set_value():
+    
+@app.route("/api/v1/empresa/<empresa_id>/historico_baixas/config", methods=["GET"])
+def get_configs(empresa_id):
+    result_set = rd_buscar_configs(empresa_id)
+    
+    if result_set:
+        return jsonify({"configs": result_set})
+    else:
+        return jsonify({"error": f"Não foi possível pegar os registros da empresa com o id: {empresa_id}"})
+
+@app.route("/api/v1/empresa/<empresa_id>/historico_baixas", methods=["POST"])
+def set_value(empresa_id):
     data = request.get_json()
 
     id_produto = data.get("id_produto")
@@ -120,30 +140,38 @@ def set_value():
     if not data_acontecimento:
         return jsonify({"error": "É necessário fornecer o campo 'data_acontecimento'"}), 400
 
-    result_set = rd_setar_registro(data) 
+    result_set, dias = rd_setar_registro(item_historico=data, empresa_id=empresa_id) 
 
     if result_set > 0:
         return jsonify({"message": f"Registro com id: {result_set} armazenado com sucesso!"})
     else:
         return jsonify({"error": f"Há algo de errado nos parâmetros do body."})
 
-@app.route("/delete/<key>", methods=["DELETE"])
-def delete_value_by_id(key):
-    result_del = rd_excluir_registro(key)
+@app.route("/api/v1/empresa/<empresa_id>/historico_baixas/<historico_id>", methods=["DELETE"])
+def delete_value_by_id(empresa_id, historico_id):
+    result_del = rd_excluir_registro(empresa_id=empresa_id, historico_id=historico_id)
     if result_del:
-        return jsonify({"message": f"Registro com id: {key} excluído com sucesso!"})
+        return jsonify({"message": f"Registro com id: {historico_id} excluído com sucesso!"})
     else:
-        return jsonify({"error": f"Registro com id: {key} não encontrada"}), 404
+        return jsonify({"error": f"Registro com id: {historico_id} não encontrada."}), 404
 
-@app.route("/get/<key>", methods=["GET"])
-def get_value_by_id(key):
-    result = rd_buscar_registro_por_id(key)
-    if result is None:
-        return jsonify({"error": f"Registro com id: {key} não encontrada"}), 404
-    return jsonify({key: result})
+@app.route("/api/v1/historico_baixas", methods=["DELETE"])
+def delete_values():
+    result_del = rd_excluir_todos_registros()
+    if result_del:
+        return jsonify({"message": f"Todos os registros excluídos!"})
+    else:
+        return jsonify({"error": f"Algum erro aconteceu e não foi possível excluir todos os registros."}), 404
 
-@app.route("/get", methods=["GET"])
-def get_values():
+@app.route("/api/v1/empresa/<empresa_id>/historico_baixas/<historico_id>", methods=["GET"])
+def get_value_by_id(empresa_id, historico_id):
+    result = rd_buscar_registro_por_id(empresa_id=empresa_id, historico_id=historico_id)
+    if len(result.values()) <= 0:
+        return jsonify({"error": f"Registro com id: {historico_id} não encontrado."}), 404
+    return jsonify({"baixa": result}   )
+
+@app.route("/api/v1/empresa/<empresa_id>/historico_baixas", methods=["GET"])
+def get_values(empresa_id):
     data = request.get_json()
     
     tipo_ordenacao = data.get("tipo_ordenacao")
@@ -151,7 +179,7 @@ def get_values():
     periodo = data.get("periodo")
 
     # Pegando todos os registros existentes
-    resultados = rd_buscar_todos_registros()
+    resultados = rd_buscar_todos_registros(empresa_id=empresa_id)
 
     return jsonify({"historico_baixas": rd_filtrar_registros(dados=resultados, tipo_registro=tipo_registro, periodo=periodo, tipo_ordenacao=tipo_ordenacao)})
 
@@ -159,8 +187,7 @@ def get_values():
 def health_check():
     return jsonify({
         "status": "ok",
-        "timestamp": datetime.now().isoformat(),
-        "uptime": "Serviço ativo"
+        "timestamp": datetime.now().isoformat()
     })
 
 if __name__ == "__main__":
